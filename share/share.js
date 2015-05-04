0a708d77371d60af94032927d483a44b8e05c91e
simenfur <jorgensen@iterate.no>
2015-05-04 09:13:05 +0200

    Update share.js

--- share/share.js
+++ share/share.js
@@ -22,8 +22,8 @@ $(function() {
     });
     
     $('.twitter').on('click', function() {
-        var loc = encodeURIComponent('http://kck.st/1bUJGKV'),
-                title = "Teach Lean Startup in 90 minutes — with this board game #playinglean http://kck.st/1bUJGKV",
+        var loc = encodeURIComponent("http://kck.st/1bUJGKV"),
+                title = "Teach Lean Startup in 90 minutes — with a board game",
                 w = 580, h = 300,
                 left = (screen.width/2)-(w/2),
                 top = (screen.height/2)-(h/2);
